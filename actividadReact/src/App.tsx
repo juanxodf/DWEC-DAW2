042f4ec8e4203010290591c6e45bccc2ebeff2e7
juanxodf <martinpozuelojuan@gmail.com>
2026-01-13 13:47:06 +0100

    feat: configuración inicial

---
+++ actividadReact/src/App.tsx
@@ -0,0 +1,23 @@
+
+
+export const MoviesApp = () => {
+  return (
+    <>
+      {/* Headers */}
+      <div className="">
+        <h1>Buscador de películas</h1>
+        <p>Descubre películas por título, género o año de estreno</p>
+      </div>
+
+      {/* Search Bar */}
+      <div className="">
+        <input type="text" placeholder="Buscar película..." />
+      </div>
+
+      {/* Previous Searches */}
+
+      {/* Movies List */}
+
+    </>
+  )
+}
