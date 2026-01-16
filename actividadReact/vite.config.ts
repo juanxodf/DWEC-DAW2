f48a9a4aa2d5cdb149bac654592665eedd109c69
juanxodf <martinpozuelojuan@gmail.com>
2026-01-16 09:10:16 +0100

    feat: añado la configuración del vitest

--- actividadReact/vite.config.ts
+++ actividadReact/vite.config.ts
@@ -1,4 +1,5 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config';
+//import react from '@vitejs/plugin-react-swc';
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
@@ -10,4 +11,10 @@ export default defineConfig({
       },
     }),
   ],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+  },
 })
+
+
